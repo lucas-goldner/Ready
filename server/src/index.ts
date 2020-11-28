8c5d26071f073da9cb620b7360f568aeab59241d
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-29 00:19:14 +0100

    Sending responses now with express

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,26 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+
+//   Add stuff with this code below
+//   const post = orm.em.create(Post, { title: "my second post" });
+//   await orm.em.persistAndFlush(post);
+
+// Read stuff with code below
+//   const posts = await orm.em.find(Post, {});
+//   console.log(posts);
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  //   const post = orm.em.create(Post, { title: "my first post" });
-  //   await orm.em.persistAndFlush(post);
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(4210, () => {
+    console.log("server started on localhost:4210");
+  });
 };
 
 main().catch((err) => {
