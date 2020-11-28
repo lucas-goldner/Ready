c7c762c72931857616115204959ab7fe7c55b51a
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-28 20:18:35 +0100

    WE DID IT MIGRATIONS WORK

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,10 +1,18 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import { MikroORM } from "@mikro-orm/core";
+import { MikroORM, Options } from "@mikro-orm/core";
+import path from "path";
 
-export default {
+const config: Options = {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [Post],
   dbName: "readydb",
   debug: !__prod__,
   type: "postgresql",
-} as Parameters<typeof MikroORM.init>[0];
+  password: "lol123",
+};
+
+export default config;
