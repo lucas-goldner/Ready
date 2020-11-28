ff7119d0aa0704782c0a65a4c4241838c4c825e9
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-29 00:09:40 +0100

    We are able to add stuff thx to mikroorm to psql

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,9 +5,11 @@ import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
-
-  const post = orm.em.create(Post, { title: "my first postt" });
-  await orm.em.persistAndFlush(post);
+  await orm.getMigrator().up();
+  //   const post = orm.em.create(Post, { title: "my first post" });
+  //   await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main().catch((err) => {
