c3df725a5b4492809f2807a2e9f0be1028285323
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-29 13:54:35 +0100

    Delete Me

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -43,4 +43,13 @@ export class PostResolver {
     }
     return post;
   }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    em.nativeDelete(Post, { id });
+    return true;
+  }
 }
