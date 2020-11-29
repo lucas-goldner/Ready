e9178efa852c4b9652fa7ec37fa52f1ea9b20705
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-29 13:36:12 +0100

    Query for single post

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Int, Arg } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
