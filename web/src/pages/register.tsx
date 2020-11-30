ffa4f2dd45d6dbdde2b48d0ef0bf33f3ef20213c
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-30 22:47:10 +0100

    Login connects to backend now

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,7 +3,8 @@ import { Formik, Form } from "formik";
 import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
 
 interface registerProps {}
 
@@ -23,13 +24,17 @@ const REGISTER_MUT = `mutation
   `;
 
 export const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUT);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register.errors) {
+            [{ field: "username", message: "something wrong" }];
+            setErrors(toErrorMap(response.data.register.errors));
+          }
         }}
       >
         {({ isSubmitting }) => (
