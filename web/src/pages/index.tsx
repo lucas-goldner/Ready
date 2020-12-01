024150aef36f335590b1c1c5cf968fac4de0e67a
Lucas Goldner <lucas.goldner@googlemail.com>
2020-12-01 17:32:59 +0100

    Posts showing up

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Navbar } from "../components/Navbar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Navbar />
+      <div>hello world</div>
+      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+    </>
+  );
+};
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
